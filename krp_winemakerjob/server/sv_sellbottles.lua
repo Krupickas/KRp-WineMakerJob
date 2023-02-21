fe063986551ecf6c43b7c8f1bcd74482ad81529e
Krupickas <114585256+Krupickas@users.noreply.github.com>
2023-02-21 19:51:00 +0100

    Update sv_sellbottles.lua

--- krp_winemakerjob/server/sv_sellbottles.lua
+++ krp_winemakerjob/server/sv_sellbottles.lua
@@ -1,46 +1,33 @@
+
 -- red wine
-RegisterServerEvent('krp:winemaker:sell:red_wine')
-AddEventHandler('krp:winemaker:sell:red_wine', function()
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-    local redw = 0
 
-	for i=1, #xPlayer.inventory, 1 do
-		local item = xPlayer.inventory[i]
 
-		if item.name == "red_wine" then
-			redw = item.count
-		end
+RegisterServerEvent("krp_winemaker:sellredwine")
+AddEventHandler("krp_winemaker:sellredwine", function(redwinequantity, sellpricetag)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local itemLabel = ESX.GetItemLabel('red_wine')
+	if xPlayer.getInventoryItem('red_wine').count >= redwinequantity then
+		xPlayer.addMoney(sellpricetag)
+		xPlayer.removeInventoryItem('red_wine', redwinequantity)
+		TriggerClientEvent("esx:showNotification",source,"You sold "..redwinequantity.."x "..'Red Wine    '.."for $"..sellpricetag.."")
+	else
+		TriggerClientEvent('esx:showNotification', source, _U('you_need_more_bottles'))
 	end
-    
-    if redw > 0 then
-        xPlayer.removeInventoryItem('red_wine', 10)
-        xPlayer.addMoney(Config.RedWinePrice)
-    else 
-		TriggerClientEvent('esx:showNotification', _source, _U('you_need_more_bottles'))
-    end
 end)
 
------white wine
 
-RegisterServerEvent('krp:winemaker:sell:white_wine')
-AddEventHandler('krp:winemaker:sell:white_wine', function()
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-    local whitew = 0
+-- red wine
 
-	for i=1, #xPlayer.inventory, 1 do
-		local item = xPlayer.inventory[i]
 
-		if item.name == "white_wine" then
-			whitew = item.count
-		end
+RegisterServerEvent("krp_winemaker:sellwhitewine")
+AddEventHandler("krp_winemaker:sellwhitewine", function(whitewinequantity, sellpricetagWhite)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local itemLabel = ESX.GetItemLabel('white_wine')
+	if xPlayer.getInventoryItem('white_wine').count >= whitewinequantity then
+		xPlayer.addMoney(sellpricetagWhite)
+		xPlayer.removeInventoryItem('white_wine', whitewinequantity)
+		TriggerClientEvent("esx:showNotification",source,"You sold ".."x "..'White wine   '.."for $"..sellpricetagWhite.."")
+	else
+		TriggerClientEvent('esx:showNotification', source, _U('you_need_more_bottles'))
 	end
-    
-    if whitew > 0 then
-        xPlayer.removeInventoryItem('white_wine', 10)
-        xPlayer.addMoney(Config.WhiteWinePrice)
-    else 
-		TriggerClientEvent('esx:showNotification', _source, _U('you_need_more_bottles'))
-    end
 end)
